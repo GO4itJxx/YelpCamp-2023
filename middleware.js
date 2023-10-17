ff974795b826db24759aee91652c624eceae1a79
Jingxi Xu <jingxi0529@gmail.com>
2023-10-17 16:42:49 -0500

    Add Controller & Star Rating

--- middleware.js
+++ middleware.js
@@ -3,9 +3,16 @@ const ExpressError = require('./utils/ExpressError');
 const Campground = require('./models/campground');
 const Review = require('./models/review');
 
+module.exports.storeReturnTo = (req, res, next) => {
+    if (req.session.returnTo) {
+        res.locals.returnTo = req.session.returnTo;
+    }
+    next();
+}
+
 module.exports.isLoggedIn = (req, res, next) => {
     if (!req.isAuthenticated()) {
-        req.session.returnTo = req.originalUrl
+        //req.session.returnTo = req.originalUrl
         req.flash('error', 'You must be signed in first!');
         return res.redirect('/login');
     }
